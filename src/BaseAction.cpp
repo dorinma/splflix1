1ea697615497af1fc257f7925c92d7d7eaf8df03
hadas <zeirah@post.bgu.ac.il>
2019-11-26 21:03:46 +0200

    baseAction commit

--- src/BaseAction.cpp
+++ src/BaseAction.cpp
@@ -3,6 +3,10 @@
 #include "../include/Action.h"
 #include "../include/Session.h"
 #include "../include/User.h"
+#include "../include/Watchable.h"
+
+
+using namespace std;
 
 
 //-----BaseAction------
@@ -13,7 +17,7 @@ void BaseAction::error(const std::string &errorMsg) {
     status = ERROR;
     this->errorMsg = errorMsg;
 }
-std::string BaseAction::getErrorMsg() const { return " Error: %n", errorMsg; }
+std::string BaseAction::getErrorMsg() const { return errorMsg;} //" Error: %n", errorMsg; }
 
 
 //-----Create user------
@@ -28,63 +32,174 @@ void CreateUser::act(Session& sess) {
     if (sess.getUserByString(name) != nullptr)
     {
         this->error("user with this name exist already");
-        return;
     }
-    if (reco == "len"){ newUser = new LengthRecommenderUser(name);}
-    else if (reco == "rer"){ newUser = new RerunRecommenderUser(name);}
-    else if (reco == "gen"){ newUser = new GenreRecommenderUser(name);}
-    else {this->error("wrong recommend was typed");}
+    else {
+        if (reco == "len") { newUser = new LengthRecommenderUser(name); }
+        else if (reco == "rer") { newUser = new RerunRecommenderUser(name); }
+        else if (reco == "gen") { newUser = new GenreRecommenderUser(name); }
+        else { this->error("wrong recommend was typed"); }
 
-    if (getStatus()!= ERROR) {
-        complete();
-        sess.addUserToMap(newUser);
-        sess.addActionToLog(this);
+        if (getStatus() != ERROR) {
+            complete();
+            sess.addUserToMap(newUser);
+            sess.addActionToLog(this);
+        }
     }
     this->toString();
 }
 std::string CreateUser::toString() const {
     if (this->getStatus() == ERROR)
-        std::cout<< this->getErrorMsg();
+        cout<< this->getErrorMsg() << endl;
     else if (this->getStatus() == COMPLETED)
-        std::cout<< "new user was created succesfuly";
+        cout<< "new user was created succesfuly" <<endl;
     else
-        std::cout<< "unkown error";
+        cout<< "unkown error" <<endl;
 }
 
 
 //------Change active user-----
 ChangeActiveUser::ChangeActiveUser () {}
-void ChangeActiveUser::act(Session& sess) {}
-std::string ChangeActiveUser::toString() const {}
+void ChangeActiveUser::act(Session& sess) {
+    std::string userInput = sess.getSessionInput();
+    std::string changedUser = userInput.substr(userInput.find(" "));
+    User *user;
+    user = sess.getUserByString(changedUser);
+    if (user == nullptr) {
+         this->error("user to change does not exist");
+    }
+    else {
+        sess.setActiveUser(user);
+        complete();
+        sess.addActionToLog(this);
+    }
+    this->toString();
+}
+std::string ChangeActiveUser::toString() const {
+    if (this->getStatus() == ERROR)
+        cout<< this->getErrorMsg() << endl;
+    else if (this->getStatus() == COMPLETED)
+        cout<< "user changed" << endl;
+    else
+        cout<< "unkown error" <<endl;
+}
 
 
 //------Delete user------
 DeleteUser::DeleteUser() {}
-void DeleteUser::act(Session& sess) {}
-std::string DeleteUser::toString() const {}
+void DeleteUser::act(Session& sess) {
+    std::string userInput = sess.getSessionInput();
+    std::string userToDelete = userInput.substr(userInput.find(" ")+1);
+    User *user;
+    user = sess.getUserByString(userToDelete);
+    if (user == nullptr){
+    this->error("user to delete does not exist");
+    }
+    else{
+        sess.setActiveUser(sess.getUserByString("default"));
+        sess.deleteUserFromMap(userToDelete);
+        complete();
+        sess.addActionToLog(this);
+    }
+    this->toString();
+}
+std::string DeleteUser::toString() const {
+    if (this->getStatus() == ERROR)
+        cout<< this->getErrorMsg()<< endl;
+    else if (this->getStatus() == COMPLETED)
+        cout<< "user deleted" << endl;
+    else
+        cout<< "unkown error" << endl;
+}
 
 
 //Duplicate user
 DuplicateUser::DuplicateUser() {}
-void DuplicateUser::act(Session& sess) {}
+void DuplicateUser::act(Session& sess) {
+    std::string userInput = sess.getSessionInput();
+    std::string oldUserName = userInput.substr(userInput.find(" "), userInput.find(" ")-1);
+    std::string newUserName = userInput.substr(userInput.find(" "));
+    User *oldUser;
+    User *newUser;
+    oldUser = sess.getUserByString(oldUserName);
+    if (oldUser == nullptr)
+    {this->error("user to duplicate from does not exist");}
+    newUser = sess.getUserByString(oldUserName);
+    if (oldUser != nullptr)
+    {this->error("this user name exist already");}
+    else {
+        newUser = oldUser;
+        complete();
+        sess.addActionToLog(this);
+    }
+    this->toString();
+}
 std::string DuplicateUser::toString() const {}
 
 
 //Print content list
 PrintContentList::PrintContentList() {}
-void PrintContentList::act(Session& sess) {}
-std::string PrintContentList::toString() const {}
+void PrintContentList::act(Session& sess) {
+
+    vector<Watchable *> contentList = sess.getContent();
+    for (int i = 0; i < contentList.size(); i++)
+    {
+        long id = contentList[i]->getId();
+        string name = contentList[i]->toString();
+        int length = contentList[i]->getLength();
+        vector<string> thisTags = contentList[i]->getTags();
+        string tags = "";
+        for (int k=0; k<thisTags.size(); k++) {
+            tags = tags + thisTags[k] + ", ";
+        }
+        tags = tags.substr(0, tags.size()-2);
+        cout << id << ". " << name << " " << length << " minutes [" << tags << "]" << endl;
+
+    }
+    complete();
+    sess.addActionToLog(this);
+}
+std::string PrintContentList::toString() const {
+    if (this->getStatus() == ERROR)
+        cout<< this->getErrorMsg()<< endl;
+    else if (this->getStatus() == COMPLETED)
+        cout<< "content was printed" << endl;
+    else
+        cout<< "unkown error" << endl;
+}
 
 
 //Print watch history
 PrintWatchHistory::PrintWatchHistory() {}
-void PrintWatchHistory::act(Session& sess) {}
+void PrintWatchHistory::act(Session& sess) {
+    string name = sess.getActiveUser()->getName();
+    cout << "Watch history for " << name << endl;
+    vector<Watchable *> history = sess.getActiveUser()->get_history();
+    //if (history.size() == 0)
+      //  cout <<"nothing was watched. you need to stop studying" << endl;
+    for (int i = 0; i < history.size(); i++)
+    {
+        long id = history[i]->getId();
+        string name = history[i]->toString();
+        cout << id << ". " << name << endl;
+
+    }
+    complete();
+    sess.addActionToLog(this);
+}
 std::string PrintWatchHistory::toString() const {}
 
 
 //Print actions log
 PrintActionsLog::PrintActionsLog() {}
-void PrintActionsLog::act(Session& sess) {}
+void PrintActionsLog::act(Session& sess) {
+    vector<BaseAction *> action = sess.getActionLog();
+    for (int i = 0; i < action.size(); i++)
+    {
+      cout << action[i]->toString();
+    }
+    complete();
+    sess.addActionToLog(this);
+}
 std::string PrintActionsLog::toString() const {}
 
 
@@ -98,4 +213,3 @@ std::string Watch::toString() const {}
 Exit::Exit() {}
 void Exit::act(Session& sess) {}
 std::string Exit::toString() const {}
-
